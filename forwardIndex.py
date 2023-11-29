377fafd9ae0078a6b7fcd52d02a7a21f630b00c6
Ryan Sikandar <119738219+RyanSikandar@users.noreply.github.com>
2023-11-29 23:48:12 +0500

    Update forwardIndex.py so that it now assigns zeros in the hit lists to words occuring in the title of the articles

--- forwardIndex.py
+++ forwardIndex.py
@@ -2,24 +2,33 @@ from nltk.tokenize import word_tokenize
 from json import load, dumps
 from LexiconModule import Lexicon
 
+# make an empty dictionary containing the forward index
 forwardIndex = {}
 
+# open the json file containing all of the articles
 with open("dataset.json", "r") as dataset:
+    # load the articles in a dictionary
     articles = load(dataset)
-    
+    # iterate through each of the articles
     for article in articles:
+        # extract a list of the word ids in the title of the article
         content_words_ids = Lexicon(word_tokenize(article["content"]))
+        # extract a list of the word ids in content of the article
         title_words_ids = Lexicon(word_tokenize(article["title"]))
+        # create an empty dictionary in the forward index for the current article id
         forwardIndex[article["id"]] = {}
+        # create empty arrays in the nested dictionary for each distinct word id
         for word_id in title_words_ids:
             forwardIndex[article["id"]][word_id] = []
         for word_id in content_words_ids:
             forwardIndex[article["id"]][word_id] = []
+        # add a '0' in the hit list for a word occuring in the title of the article
         for word_id in title_words_ids:
             forwardIndex[article["id"]][word_id].append(0)
+        # add the postion of the word occuring in the content of the article in the hit list
         for index, word_id in enumerate(content_words_ids):
             forwardIndex[article["id"]][word_id].append(index)
 
-
+# write the forward index to a json file
 with open("forwardIndex.json", "w") as ForwardIndex:
     ForwardIndex.write(dumps(forwardIndex))
