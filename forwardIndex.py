c59a1f9a17f1f41e1e47c858302e71e0705a93f9
Waqas Ali Qureshi <131449899+waqas2714@users.noreply.github.com>
2023-11-29 23:39:02 +0500

    Updated forwardIndex Code

--- forwardIndex.py
+++ forwardIndex.py
@@ -6,10 +6,8 @@ forwardIndex = {}
 
 with open("dataset.json", "r") as dataset:
     articles = load(dataset)
-    i = 0
+    
     for article in articles:
-        if i >= 5:
-            break
         content_words_ids = Lexicon(word_tokenize(article["content"]))
         title_words_ids = Lexicon(word_tokenize(article["title"]))
         forwardIndex[article["id"]] = {}
@@ -19,10 +17,9 @@ with open("dataset.json", "r") as dataset:
             forwardIndex[article["id"]][word_id] = []
         for word_id in title_words_ids:
             forwardIndex[article["id"]][word_id].append(0)
-        for index, word_id in enumerate(title_words_ids):
+        for index, word_id in enumerate(content_words_ids):
             forwardIndex[article["id"]][word_id].append(index)
 
-print(forwardIndex)
 
 with open("forwardIndex.json", "w") as ForwardIndex:
     ForwardIndex.write(dumps(forwardIndex))
