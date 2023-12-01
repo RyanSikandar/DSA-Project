be0515a2d429760f80ab0626191c00e375de1b17
waqas2714 <waqasali00123@gmail.com>
2023-12-01 17:45:38 +0500

    forward index made

--- forwardIndex.py
+++ forwardIndex.py
@@ -5,15 +5,12 @@ from LexiconModule import Lexicon
 # make an empty dictionary containing the forward index
 forwardIndex = {}
 
-i = 1
 # open the json file containing all of the articles
 with open("dataset.json", "r") as dataset:
     # load the articles in a dictionary
     articles = load(dataset)
     # iterate through each of the articles
-    for article in articles:
-        if(i > 2):
-            break                                                                   #ARTICLE
+    for article in articles:                                                    #ARTICLE
         # extract a list of the word ids in the title of the article
         content_words_ids = Lexicon(word_tokenize(article["content"]))
         # extract a list of the word ids in content of the article
@@ -31,7 +28,6 @@ with open("dataset.json", "r") as dataset:
         # add the postion of the word occuring in the content of the article in the hit list
         for index, word_id in enumerate(content_words_ids):
             forwardIndex[article["id"]][content_words_ids[word_id]].append(index)
-        i += 1
 # write the forward index to a json file
 with open("forwardIndex.json", "w") as ForwardIndex:
     ForwardIndex.write(dumps(forwardIndex))
