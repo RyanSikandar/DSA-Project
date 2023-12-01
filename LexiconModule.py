ad825b2a37ebd602ee59a6ed91b8d9597c6fbeb5
saad1551 <saad.ashraf.9094@gmail.com>
2023-12-01 21:19:22 +0500

    idk

--- LexiconModule.py
+++ LexiconModule.py
@@ -2,15 +2,15 @@ from json import load
 
 # a reusable function that takes a list of words and returns a dictionary where the keys are the words and the values are the IDS
 def Lexicon(word_list):
-    # a dictionary that will be returned
-    word_id_dict = {}
+    # a list that will be returned
+    word_ids = []
     # opening the lexicon json file
     with open("lexicon.json", "r") as LexiconFile:
         # loading the file in a dictionary
         lexiconDict = load(LexiconFile)
         # iterates through each word in the incoming list
         for word in word_list:
-            # creates a key value pair extracting the ID of the word from the larger lexicon
-            word_id_dict[word] = lexiconDict[word]
-    #returns the dictionary
-    return word_id_dict
+            # appends the word id to the list
+            word_ids.append(lexiconDict[word])
+    #returns the list
+    return word_ids
