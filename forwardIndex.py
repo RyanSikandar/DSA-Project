657f3acb798d28aa0a2b4fc85bda59859c3e8d21
waqas2714 <waqasali00123@gmail.com>
2023-12-03 23:15:22 +0500

    changed the forward index so that it now contains both the weightage scores and the hitlists for each word in a document

--- forwardIndex.py
+++ forwardIndex.py
@@ -19,27 +19,33 @@ with open("dataset.json", "r") as dataset:
         forwardIndex[article_id] = {}
         # set the weightage of each word in the document to 0
         for word_id in title_words_ids:
-            forwardIndex[article_id][word_id] = 0
+            forwardIndex[article_id][word_id] = [0, []]
         for word_id in content_words_ids:
-            forwardIndex[article_id][word_id] = 0
-        # add 25 to the weightage score for a word occuring in the title of the article
+            forwardIndex[article_id][word_id] = [0, []]
+        # add 25 to the weightage score for a word occuring in the title of the article and add -1 to the hitlist
         for word_id in title_words_ids:
-            forwardIndex[article_id][word_id] += 25
+            forwardIndex[article_id][word_id][0] += 25
+            forwardIndex[article_id][word_id][1].append(-1)
         for index, word_id in enumerate(content_words_ids):
             # add 10 to the weightage score for a word occuring in the first 30 words of the content of the article
             if index < 30:
-                forwardIndex[article_id][word_id] += 10
+                forwardIndex[article_id][word_id][0] += 10
             # add 5 to the weightage score for a word occuring between postions 30 and 70 in the content of the article
             elif index < 70:
-                forwardIndex[article_id][word_id] += 5
+                forwardIndex[article_id][word_id][0] += 5
             # add 5 to the weightage score for a word occuring between postions 70 and 150 in the content of the article
             elif index < 150:
-                forwardIndex[article_id][word_id] += 2
+                forwardIndex[article_id][word_id][0] += 2
             # add 1 to the weightage score for a word occuring after postion 150 in the content of the article
             else:
-                forwardIndex[article_id][word_id] += 1
+                forwardIndex[article_id][word_id][0] += 1
+            # add the position of occurence of the word in the hitlist
+            forwardIndex[article_id][word_id][1].append(index)
 
 # write the forward index to a json file
 with open("forwardIndex.json", "w") as ForwardIndex:
     ForwardIndex.write(dumps(forwardIndex))
+
+
+
 
--- invertedIndex.py
+++ invertedIndex.py
@@ -1,15 +1,20 @@
 from json import load, dumps
 
+# create a dictionary to store the inverted index
 invertedIndex = {}
 
 # open the Forward Index file and read the data into a variable
 with open ("forwardIndex.json", "r") as ForwardIndexFile:
     forwardIndex = load(ForwardIndexFile)
+    # loop through the key value pairs in the outer dictionary
     for document_id, words_data in forwardIndex.items():
-        for word_id, weightage in words_data.items():
+        # for each key value pair in the outer dictionary, loop through the key value pairs in the inner dictionary
+        for word_id, weightageAndHitList in words_data.items():
+            # if the word doesn't already exist in the inverted index, create an empty dictionary against that word id in the inverted index
             if word_id not in invertedIndex:
                 invertedIndex[word_id] = {}
-            invertedIndex[word_id][document_id] = weightage
+            # add the document id and the weightage score in the dictionary of that word id
+            invertedIndex[word_id][document_id] = weightageAndHitList
 
 with open("invertedIndex.json", "w") as InvertedIndexFile:
     InvertedIndexFile.write(dumps(invertedIndex))
