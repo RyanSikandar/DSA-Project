6ea63c624deb9a089ab5d11c3e7f28a3011d0adb
waqas2714 <waqasali00123@gmail.com>
2023-12-08 12:43:13 +0500

    Barrels Created but there are some faults

---
+++ createBarrels.py
@@ -0,0 +1,29 @@
+import json
+import os
+
+# Create a directory for barrels if it doesn't exist
+if not os.path.exists("barrels"):
+    os.makedirs("barrels")
+
+# Read data from the invertedIndex.json file
+with open('invertedIndex.json', 'r') as file:
+    data = json.load(file)
+
+# Iterate through the sorted keys in batches of 2500 and create barrels
+barrel_count = 1
+object_count = 0
+objects = {}
+
+for key in sorted(data.keys()):
+    objects[key] = data[key]
+    object_count += 1
+
+    # If 2500 objects are reached or it's the last object, create a barrel
+    if object_count == 2500 or key == sorted(data.keys())[-1]:
+        barrel_file_name = f"barrels/{list(objects.keys())[0]}-{list(objects.keys())[-1]}.json"
+        with open(barrel_file_name, 'w') as barrel_file:
+            json.dump(objects, barrel_file, indent=4)
+        # Reset variables for the next barrel
+        object_count = 0
+        objects = {}
+        barrel_count += 1
