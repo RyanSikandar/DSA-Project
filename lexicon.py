f26f147077e26517b01c586f214b7884f36894a4
waqas2714 <waqasali00123@gmail.com>
2023-12-08 11:50:07 +0500

    changing lexicon and assigning meaningful ids

--- lexicon.py
+++ lexicon.py
@@ -4,8 +4,14 @@ from nltk.tokenize import word_tokenize
 # a dictionary where the words will be the key and their ids will be the values
 lexicon = {}
 
-# unique integral ids to give to each word
-id = 1
+# FUNCTION THAT GENERATES IDs FOR WORDS.
+def calculate_word_id(word):
+    word_id = 1
+    for index, char in enumerate(word):
+        char_value = ord(char) * (index + 1)
+        word_id *= char_value
+    return word_id
+
 
 # open the json file containing all of the articles
 with open("dataset.json", "r") as dataset:
@@ -16,16 +22,15 @@ with open("dataset.json", "r") as dataset:
         # stores the words of the title and the content in a list
         title = word_tokenize(article["title"])
         content = word_tokenize(article["content"])
-        '''iterates through each word, gives it an ID, stores it in the lexicon dictionary 
-        if it is not already there, and increments the ID counter by 1 '''
+        # '''iterates through each word, gives it an ID, stores it in the lexicon dictionary 
+        # if it is not already there, and increments the ID counter by 1 '''
         for word in title:
             if not word in lexicon:
-                lexicon[word] = id
-                id += 1
+                lexicon[word] = calculate_word_id(word)
         for word in content:
             if not word in lexicon:
-                lexicon[word] = id
-                id += 1
+                lexicon[word] = calculate_word_id(word)
+
 
 
 with open("lexicon.json", "w") as Lexicon:
